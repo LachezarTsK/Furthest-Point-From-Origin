d6000c4633fb807d63cde0e0271e150a34868bf0
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-09 21:06:10 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,24 @@
+
+function furthestDistanceFromOrigin(moves: string): number {
+    const LEFT_MOVE = 'L';
+    const RIGHT_MOVE = 'R';
+    const OPTIONAL_LEFT_OR_RIGHT_MOVE = '_';
+
+    let leftMoves = 0;
+    let rightMoves = 0;
+
+    for (let move of moves) {
+        if (move === LEFT_MOVE) {
+            ++leftMoves;
+        }
+        if (move === RIGHT_MOVE) {
+            ++rightMoves;
+        }
+    }
+
+    let optionalMoves = moves.length - leftMoves - rightMoves;
+    let directionOptionalMoves = (leftMoves > rightMoves) ? -1 : 1;
+    let furthestPointFromOrigin = Math.abs(rightMoves - leftMoves + (directionOptionalMoves * optionalMoves));
+
+    return furthestPointFromOrigin;
+};
